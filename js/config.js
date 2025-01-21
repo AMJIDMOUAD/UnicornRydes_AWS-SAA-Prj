4913e84da9a3f328810a3730a9026866e05eb83a
AMJIDMOUAD <52169428+AMJIDMOUAD@users.noreply.github.com>
2025-01-21 17:10:43 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_rUYSQBUVC' , // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4fjsgf4v7qnkgrtv15qd0m7eci', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_X6lUfxMcs' , // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2u7ogumfs35g1g1ii8f1tlmcg5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
