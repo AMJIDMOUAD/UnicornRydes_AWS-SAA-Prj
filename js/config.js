c6c1911bb69b763af3c59065734bf71eceeacda3
AMJIDMOUAD <52169428+AMJIDMOUAD@users.noreply.github.com>
2025-01-21 18:21:30 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://3g1kkgh9j5.execute-api.us-east-1.amazonaws.com/po' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://yw1oo3dpfk.execute-api.us-east-1.amazonaws.com/api' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
